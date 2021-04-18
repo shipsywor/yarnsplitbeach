d7f053534fae2ff5c76e4b77e1bddde08e777565
shipsywor <shipsyworgmail.com>
2021-04-18 18:39:56 +0900

    Updated

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Link } from './link';
+export { default as Layout } from './layout';
+export { default as ThemeProvider } from './theme/themeProvider';
+export { default as mdxComponents } from './mdxComponents';
+export { default } from './theme';
